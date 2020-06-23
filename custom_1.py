d4aa12869e51ce826bf73217983438556c05836a
Gabriel <gabriel.sales_pompeu@hotmail.com>
2020-06-22 23:20:47 -0300

    consertando path getcwd

--- custom_1.py
+++ custom_1.py
@@ -39,7 +39,7 @@ import matplotlib.pyplot as plt
 
 # Root directory of the project
 # ROOT_DIR = 'C:/Users/Sourish/Mask_RCNN/'
-ROOT_DIR = os.getcwd() + '/'
+ROOT_DIR = os.getcwd() + '//'
 
 # Import Mask RCNN
 sys.path.append(ROOT_DIR)  # To find local version of the library
